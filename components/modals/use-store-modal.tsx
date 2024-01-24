11eaa9a69d544405b146220c7fbde9b25f5dbeb5
aaronwai <aaron.lung.wai@gmail.com>
2024-01-24 21:39:58 +0800

    Step 3 : zustand

---
+++ components/modals/use-store-modal.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Modal } from "../ui/modal";
+export const StoreModal = () => {
+  <Modal
+    title='Create store'
+    description='Add a new store to manage products and categories.'
+    isOpen={false}
+    onClose={() => {}}
+  >
+    Future Create Store
+  </Modal>;
+};
