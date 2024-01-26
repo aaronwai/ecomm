55f689ee539c961899134aa040ddc364f3280852
aaronwai <aaron.lung.wai@gmail.com>
2024-01-26 20:43:20 +0800

    Step 9 : dashbaord basic

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>
+        <body className={inter.className} suppressHydrationWarning={true}>
           <ToastProvider />
           <ModalProvider />
           {children}
