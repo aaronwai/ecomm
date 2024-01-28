7672e60f419adeb1ed9006a8294fae5eed3400af
aaronwai <aaron.lung.wai@gmail.com>
2024-01-28 21:09:45 +0800

    Step 16 : cell-action function

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -8,6 +8,7 @@ import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { columns, BillboardColumn } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -34,6 +35,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       <Heading title='API' description='API Calls for Billboards' />
       <Separator />
       <DataTable searchKey='label' columns={columns} data={data} />
+      <Heading title='API' description='API Calls for Billboards' />
+      <Separator />
+      <ApiList entityName='billboards' entityIdName='billboardId' />
     </>
   );
 };
